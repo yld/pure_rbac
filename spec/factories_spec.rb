acf449938f531880d0fc0b467f4331c999da67fa
Yves Le Douaron <yves@finalcad.com>
2018-06-16 21:22:45 +0200

    clean up factories lint

--- spec/factories_spec.rb
+++ spec/factories_spec.rb
@@ -1,26 +1,12 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
-# Patch for FactoryBot::Linter. Clean Database before each factory if strategy is :create
-class FactoryBot::Linter
-  private
-  # access to original method.
-  alias_method :original_lint_factory_and_traits, :lint_factory_and_traits
-
-  def lint_factory_and_traits(factory)
-    begin
-      DatabaseCleaner.start if factory_strategy == :create
-      original_lint_factory_and_traits(factory)
-    ensure
-      DatabaseCleaner.clean if factory_strategy == :create
-    end
-  end
-end
-
 describe 'FactoryBot Linter' do
-  [:build_stubbed, :build, :create].each do |strategy|
-    it "#{strategy}" do
+  %i[build_stubbed build create].each do |strategy|
+    it "#{strategy} factories" do
       DatabaseCleaner.cleaning do
-        FactoryBot::Linter.new(FactoryBot.factories, :factory_and_traits, strategy).lint!
+        FactoryBot.lint traits: true, strategy: strategy
       end
     end
   end
